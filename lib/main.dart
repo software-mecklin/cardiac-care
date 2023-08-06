c160c37ab07ff80901889395d0834eef805867f3
shuvambag <shuvambag@gmail.com>
2023-08-07 03:19:01 +0530

    Fixed transitions and greetings

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:medcentral/pages/splash_screen.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.env';
+import 'firebase_options.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
